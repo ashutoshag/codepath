a7347715a49d89da045bd84f9ad9cf4e7f049603
Ashutosh Agarwal <ashutoshag87@gmail.com>
2017-07-08 13:20:07 -0700

    Adds a settings view controller, settings button and an empty settings view to the main screen.

---
+++ TipCalculator/SettingsViewController.swift
@@ -0,0 +1,27 @@
+//
+//  SettingsViewController.swift
+//  TipCalculator
+//
+//  Created by Ashutosh Agarwal on 7/8/17.
+//  Copyright © 2017 Ashutosh Agarwal. All rights reserved.
+//
+
+import UIKit
+
+class SettingsViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Uncomment the following line to preserve selection between presentations
+        // self.clearsSelectionOnViewWillAppear = false
+
+        // Uncomment the following line to display an Edit button in the navigation bar for this view controller.
+        // self.navigationItem.rightBarButtonItem = self.editButtonItem()
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+}

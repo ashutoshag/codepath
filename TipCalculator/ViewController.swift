be645cb4400a4142e7f41e0ebf9541f3ebd4492c
Ashutosh Agarwal <ashutoshag87@gmail.com>
2017-07-08 13:07:38 -0700

    Fully working calculator app.

--- TipCalculator/ViewController.swift
+++ TipCalculator/ViewController.swift
@@ -10,6 +10,12 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var BillTextField: UITextField!
+    @IBOutlet weak var TotalLabel: UILabel!
+    @IBOutlet weak var TipLabel: UILabel!
+    
+    @IBOutlet weak var TipControl: UISegmentedControl!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
@@ -20,6 +26,22 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
+    @IBAction func onTap(_ sender: AnyObject) {
+        print("Hello")
+        view.endEditing(true);
+    }
 
+    @IBAction func tipPercentageChanged(_ sender: AnyObject) {
+        calculateTip(AnyObject.self);
+    }
+    
+    @IBAction func calculateTip(_ sender: Any) {
+        let tipPercentages = [0.18, 0.20, 0.25];
+        let bill = Double(BillTextField.text!) ?? 0;
+        let tip = bill * tipPercentages[TipControl.selectedSegmentIndex];
+        let total = bill + tip;
+        TipLabel.text = String(format: "$%.2f", tip)
+        TotalLabel.text = String(format: "$%.2f", total)
+    }
 }
 

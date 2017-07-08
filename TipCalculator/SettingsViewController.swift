731ad231ba52acae3cc63012e2520b9dbe052918
Ashutosh Agarwal <ashutoshag87@gmail.com>
2017-07-08 13:54:50 -0700

    Save choice of default tip to UserDefaults.

--- TipCalculator/SettingsViewController.swift
+++ TipCalculator/SettingsViewController.swift
@@ -10,6 +10,10 @@ import UIKit
 
 class SettingsViewController: UIViewController {
 
+    @IBOutlet weak var DefaultTipController: UISegmentedControl!
+    
+    let TIP_SEGMENT_KEY =  "default_tip_segment_index";
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
@@ -18,10 +22,42 @@ class SettingsViewController: UIViewController {
 
         // Uncomment the following line to display an Edit button in the navigation bar for this view controller.
         // self.navigationItem.rightBarButtonItem = self.editButtonItem()
+        getSavedTipChoice()
+    }
+    
+    override func viewWillAppear(_ animated: Bool) {
+        getSavedTipChoice()
+    }
+    
+    override func viewDidAppear(_ animated: Bool) {
+        getSavedTipChoice()
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
+    }
+    
+    override func viewWillDisappear(_ animated: Bool) {
+        saveSelectedSegmentIndex();
+    }
+    
+    @IBAction func onDefaultTipChoiceChange(_ sender: Any) {
+        saveSelectedSegmentIndex()
+    }
+    
+    func saveSelectedSegmentIndex() {
+        let index = DefaultTipController.selectedSegmentIndex
+        let defaults = UserDefaults.standard
+        defaults.set(index, forKey: TIP_SEGMENT_KEY)
+        defaults.synchronize()
+    }
+    
+    func getSavedTipChoice() {
+        let defaults = UserDefaults.standard
+        let index = defaults.object(forKey: TIP_SEGMENT_KEY)
+        let intIndex = (index as? Int) ?? 0
+        print(String(format: "Selected default index: %d", intIndex))
+        DefaultTipController.setEnabled(true, forSegmentAt: intIndex);
     }
 }
